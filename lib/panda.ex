b335acccdcb322715fc93fc0cb39712747195489
Ivica Arsov <ivica.arsov@gmail.com>
2019-03-18 17:05:34 +0100

    Refactor api call in another function

--- lib/panda.ex
+++ lib/panda.ex
@@ -1,11 +1,15 @@
 defmodule Panda do
-  def upcoming_matches() do
+
+  def api_call(url) do
     token = Application.get_env(:panda, :api_key)
-    url = "https://api.pandascore.co/matches/upcoming?per_page=5&order=begin_at"
+    url = "https://api.pandascore.co/#{url}"
     headers = ["Authorization": "Bearer #{token}", "Accept": "Application/json; Charset=utf-8"]
     {:ok, response} = HTTPoison.get(url, headers)
-    response.body()
-      |> Poison.decode!
+    response.body() |> Poison.decode!
+  end
+
+  def upcoming_matches() do
+    api_call("matches/upcoming?per_page=5&order=begin_at")
       |> Enum.map(fn(val) -> Map.take(val, ["begin_at", "id", "name"]) end)
   end
 end
