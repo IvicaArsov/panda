c21fe0f1c44f46656fc0ffc3d7f9d85bf243770c
Ivica Arsov <ivica.arsov@gmail.com>
2019-03-24 20:51:27 +0100

    Refactor function to return total as well

--- lib/panda.ex
+++ lib/panda.ex
@@ -5,11 +5,15 @@ defmodule Panda do
     url = "https://api.pandascore.co/#{url}"
     headers = ["Authorization": "Bearer #{token}", "Accept": "Application/json; Charset=utf-8"]
     {:ok, response} = HTTPoison.get(url, headers)
-    response.body() |> Poison.decode!
+    %{
+      "body" => Poison.decode!(response.body()),
+      "total" => List.keyfind(response.headers, "X-Total", 0, {"X-Total", "1"}) |> elem(1)
+    }
   end
 
   def upcoming_matches() do
     api_call("matches/upcoming?per_page=5&order=begin_at")
+      |> Map.fetch!("body")
       |> Enum.map(fn(val) -> Map.take(val, ["begin_at", "id", "name"]) end)
   end
 end
